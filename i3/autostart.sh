a9dd7a87739d796a802c380b5358ca5be1eaf28d
Youssef Eddaif <jo9unreal@gmail.com>
2023-02-02 22:14:08 +0100

    fix xdg-desktop-portal issue and some minor changes

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 picom --config ~/.config/picom/picom.conf &
+setxkbmap fr
+xfce4-power-manager &
 $HOME/.config/polybar/launcher.sh &
 ~/.fehbg &
+exec /usr/libexec/xdg-desktop-portal-gtk &
 dunst &
 nm-applet &
 lxpolkit &
-xfce4-power-manager &
 xgamma -gamma 0.8
+brightnessctl s 472
