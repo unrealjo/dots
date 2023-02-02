7a7f024187b530e67639637a4450eec19e8e927e
Youssef Eddaif <jo9unreal@gmail.com>
2023-02-02 22:10:32 +0100

    organize plugins

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,34 +1,37 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use('wbthomason/packer.nvim')
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use('tpope/vim-fugitive')
-		-- LSP Support
-	use('neovim/nvim-lspconfig')
-		-- Autocompletion
-	use('hrsh7th/nvim-cmp')
-	use('hrsh7th/cmp-buffer')
-	use('hrsh7th/cmp-path')
-	use('hrsh7th/cmp-nvim-lsp')
-	use('hrsh7th/cmp-nvim-lua')
-		 -- Snippets
-	use('L3MON4D3/LuaSnip')
-	use('saadparwaiz1/cmp_luasnip')
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('tpope/vim-fugitive')
+    -- LSP Support
+    use('neovim/nvim-lspconfig')
+    -- Autocompletion
+    use('hrsh7th/nvim-cmp')
+    use('hrsh7th/cmp-buffer')
+    use('hrsh7th/cmp-path')
+    use('hrsh7th/cmp-nvim-lsp')
+    use('hrsh7th/cmp-nvim-lua')
+    -- Snippets
+    use('L3MON4D3/LuaSnip')
+    use('saadparwaiz1/cmp_luasnip')
+    use { "ellisonleao/gruvbox.nvim" }
     use('navarasu/onedark.nvim')
     use {
-     'nvim-lualine/lualine.nvim',
-     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
     use { "nvim-telescope/telescope-file-browser.nvim" }
     use('MunifTanjim/prettier.nvim')
+    -- using packer.nvim
+    use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
     use {
-	"windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
     }
 end)
