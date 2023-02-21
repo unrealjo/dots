b9c2ae765094b723c98ab2b7e840a4ce0ead0b83
Youssef Eddaif <jo9unreal@gmail.com>
2023-02-21 22:36:29 +0100

    change the colorsheme to ondarkpro

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,1 @@
---require('onedark').setup {
---    style = 'deep',
---   transparent = true,
---}
---require('onedark').load()
-require("gruvbox").setup({
-  contrast = "hard", -- can be "hard", "soft" or empty string
-})
-vim.cmd([[colorscheme gruvbox]])
+vim.cmd("colorscheme onedark_vivid")
