951ec82121fabaabd706dacc9e870edd36fcefd0
Youssef Eddaif <jo9unreal@gmail.com>
2023-02-21 22:57:41 +0100

    format files : lualine.lua, prettier.lua and treesitter.lua

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,38 +1,38 @@
 local diagnostics = {
     "diagnostics",
-    sources = {"nvim_diagnostic"},
-    sections = {"error", "warn"},
-    symbols = {error = " ", warn = " "},
+    sources = { "nvim_diagnostic" },
+    sections = { "error", "warn" },
+    symbols = { error = " ", warn = " " },
     colored = false,
     update_in_insert = false,
     always_visible = true
 }
 require('lualine').setup {
-  options = {
-    theme = "auto",
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+    options = {
+        theme = "auto",
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
     },
-    lualine_b = { 'filename', 'branch' ,diagnostics},
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+    sections = {
+        lualine_a = {
+            { 'mode', separator = { left = '' }, right_padding = 2 },
+        },
+        lualine_b = { 'filename', 'branch', diagnostics },
+        lualine_c = { 'fileformat' },
+        lualine_x = {},
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+        },
     },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
+    inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
 }
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,28 +1,31 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+    return
 end
 
 configs.setup {
-  ensure_installed = {"yaml","html","bash","go","javascript","python","css","php","typescript","lua","c","markdown"},
-  sync_install = false,
-  ignore_install = { "" },
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true,
-    disable = { "" },
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+    ensure_installed = { "yaml", "html", "bash",
+        "go", "javascript", "python",
+        "css", "php", "typescript",
+        "lua", "c", "markdown"
+    },
+    sync_install = false,
+    ignore_install = { "" },
+    autopairs = {
+        enable = true,
+    },
+    highlight = {
+        enable = true,
+        disable = { "" },
+        additional_vim_regex_highlighting = true,
+    },
+    indent = { enable = true, disable = { "yaml" } },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+    },
     autotag = {
-    enable = true,
-    filetypes = { "html" , "xml","php" },
-  }
+        enable = true,
+        filetypes = { "html", "xml", "php" },
+    }
 }
-
